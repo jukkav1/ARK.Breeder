5480166bf7d9f0c49040e5a7f8126a713fd45022
Jukka V <126549684+jukkav1@users.noreply.github.com>
2023-03-24 17:18:13 +0200

    -moved function out from while loop

--- mainV5.py
+++ mainV5.py
@@ -1,42 +1,44 @@
 from colorama import Fore
 
-FORES=[Fore.RED,Fore.BLUE,Fore.YELLOW,Fore.GREEN]
+FORES = [Fore.RED, Fore.BLUE, Fore.YELLOW, Fore.GREEN]
+
+
+def colored_stats(s, color=Fore.RED, **kwargs):
+    print(f"{color}{s}")
+    healt = input("Healt:")
+    if stats == "a":
+        oxygen = input("Oxygen:")
+
 
 while True:
     print("")
-    stats=input(Fore.RESET+"All or important stats:(A/I), (Q) for quit:")
-    stats=stats.lower()
-    
-    if stats=="q":
+    stats = input(Fore.RESET + "All or important stats:(A/I), (Q) for quit:")
+    stats = stats.lower()
+
+    if stats == "q":
         print("See you later, mate!")
         break
-    elif stats =="a" or stats=="i":
-        amount=input("How many creatures?(1-4):")
+    elif stats == "a" or stats == "i":
+        amount = input("How many creatures?(1-4):")
         try:
-            amount=int(amount)
+            amount = int(amount)
         except ValueError:
             print("")
-            print(Fore.MAGENTA+"Wrong input, try again!")
+            print(Fore.MAGENTA + "Wrong input, try again!")
             continue
 
-        if amount<=0 or amount>4:
+        if amount <= 0 or amount > 4:
             print("")
-            print(Fore.MAGENTA+"Wrong input, try again!")
+            print(Fore.MAGENTA + "Wrong input, try again!")
         else:
-            while amount>0:
-                creature=input("Creature:")
-                def colored_stats(s,color=Fore.RED,**kwargs):
-                    print(f"{color}{s}")
-                    healt=input("Healt:")
-                    if stats=="a":
-                        oxygen=input("Oxygen:")
-    
+            while amount > 0:
+                creature = input("Creature:")
                 for fore in FORES:
                     print("")
-                    colored_stats(f"Creature:{creature}",color=fore)
-                    amount-=1
-                    if amount==0:
+                    colored_stats(f"Creature:{creature}", color=fore)
+                    amount -= 1
+                    if amount == 0:
                         break
     else:
         print("")
-        print(Fore.MAGENTA+"Wrong input, try again!")
+        print(Fore.MAGENTA + "Wrong input, try again!")
